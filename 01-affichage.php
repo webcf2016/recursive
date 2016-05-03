9a1b6b253e014c5bf1e5d0a9595b1b468f2acae0
Mikhawa <michaeljpitz@gmail.com>
2016-05-03 14:31:26 +0200

    premiers fichiers

---
+++ 01-affichage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Affichage de nos fonctions récursives</title>
+</head>
+<body>
+<h1>Affichage de nos fonctions récursives</h1>
+<h2>First</h2>
+<p>Une fonction récursive est appelée par elle-même. D'une certaine manière il s'agit d'une forme de boucle. ! La fonction récursive ne doit pas être infinie<br/>Pour créer un menu multiniveaux sans connaître le nombre et la profondeur de chaque rubrique, elle est indispensable!</p>
+</body>
+</html>
---
+++ 01-fonction.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webform
+ * Date: 3/05/2016
+ * Time: 14:26
+ */
